ea6f8ea8a491ecfd7753e2629fe87550a1ce1663
Griffin <348875758@gapps.yrdsb.ca>
2026-06-15 11:34:30 -0400

    Updated fizzbuzz to apply to web instead of just console.  Used CSS in my html

---
+++ database.js
@@ -0,0 +1,7 @@
+const inventory = [
+    {name: "Oreos", quantity: 10},
+    {name: "Chips-a-hoy", quantity: 8},
+    {name: "Goldfish", quantity: 8}
+]
+
+console.log(inventory[0])
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,24 @@
+const targetElement = document.getElementById("fizzbuzz"); // Find the HTML element with the ID "hello-world" to connect
+
 function fizzbuzz() {
-    for (let i = 1; i < 51; i++) //for loop in js with three parameters
+    for (let i = 1; i < 51; i++) {//for loop in js with three parameters
         if (i % 3 === 0 && i % 5 === 0) { //Check if divisible by 3 and 5, if so logs FizzBuzz
             console.log("FizzBuzz");
+            targetElement.textContent += "FizzBuzz" + "\n"; 
         }
         else if (i % 3 === 0) { //Check if divisible by 3, if so logs Fizz
-            console.log("Fizz")
+            console.log("Fizz");
+            targetElement.textContent += "Fizz" + "\n";
         }
         else if (i % 5 === 0) { //Check if divisible by 5, if so logs Buzz
-            console.log("Buzz")
+            console.log("Buzz");
+            targetElement.textContent += "Buzz" + "\n";
         }
         else {
-            console.log(i) //Otherwise logs the number
+            console.log(i); //Otherwise logs the number
+            targetElement.textContent += i+ "\n"; //Concatinate so it reads with newline
         }
+    }
 }
 
 fizzbuzz();
