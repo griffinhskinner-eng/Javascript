343839b8d7ad1a7bdf99702390f865a507c67a40
Griffin <348875758@gapps.yrdsb.ca>
2026-06-15 11:50:45 -0400

    Made a database of users using an array of objects with key-vaue pairs

--- database.js
+++ database.js
@@ -1,7 +1,29 @@
-const inventory = [
-    {name: "Oreos", quantity: 10},
-    {name: "Chips-a-hoy", quantity: 8},
-    {name: "Goldfish", quantity: 8}
+//The javascrit equivalent of a python dictionary is called an object.  Its similar in the sense that it stores value key-value pairs 
+
+const users = [ //const means constant.  I can't change the variable name users but I can change and append/remove data inside
+    { //Curly brackets are objects, within an array (square brackets)
+        username: "Gskinns08",
+        password: "876682",
+        age: 17,
+
+    },
+    {
+        username: "Mr.Deboomer",
+        password: "978531",
+        age: 24,
+
+    },
+    {
+        username: "BGonns1",
+        password: "286103",
+        age: 18,
+
+    },
+    {
+        username: "JFaz!",
+        password: "713490",
+        age: 20,
+
+    },
 ]
 
-console.log(inventory[0])
