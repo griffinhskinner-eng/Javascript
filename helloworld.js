f19212c16e6395adc2295bc4ce8905e9167f18be
Griffin <348875758@gapps.yrdsb.ca>
2026-06-14 21:26:24 -0400

    Added console.log hello world message

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,2 @@
-#helloworld!
-#another line
-test again
+console.log("hello world from console"); //console.log puts writing down in the console of a web page.  To be accessed by right clicking and going to inspect, then console
+
