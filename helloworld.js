3f03231263738cb3fdc68efa807eb7ed762409c3
Griffin <348875758@gapps.yrdsb.ca>
2026-06-14 22:00:10 -0400

    Commented out when each line does

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,4 @@
 console.log("hello world from console"); //console.log puts writing down in the console of a web page.  To be accessed by right clicking and going to inspect, then console
 
+const targetElement = document.getElementById("hello-world"); // Find the HTML element with the ID "hello-world" to connect
+targetElement.textContent = "Hello world from JavaScript file."; //Print text content, "Hello World from Javascript file"
