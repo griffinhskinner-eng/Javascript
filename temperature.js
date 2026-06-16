d3fa9e6eec3e8e997cf50014da35784db3cf1fb6
Griffin <348875758@gapps.yrdsb.ca>
2026-06-16 12:02:38 -0400

    Finished temperature function

---
+++ temperature.js
@@ -0,0 +1,49 @@
+const temperatureInput = document.getElementById("temperature");
+const fromUnit = document.getElementById("fromUnit");
+const toUnit = document.getElementById("toUnit");
+const convertButton = document.getElementById("convertButton");
+const result = document.getElementById("result");
+
+function convertTemperature() {
+    const temperature = Number(temperatureInput.value);
+
+    if (fromUnit.value === "C" && toUnit.value === "C") {
+        result.textContent = temperature + "°C";
+    }
+
+    else if (fromUnit.value === "C" && toUnit.value === "F") {
+        result.textContent = temperature * 9 / 5 + 32 + "°F";
+    }
+
+    else if (fromUnit.value === "C" && toUnit.value === "K") {
+        result.textContent = temperature + 273.15 + "°K";
+    }
+
+    else if (fromUnit.value === "F" && toUnit.value === "C") {
+        result.textContent = (temperature - 32) * 5 / 9 + "°C";
+    }
+
+    else if (fromUnit.value === "F" && toUnit.value === "F") {
+        result.textContent = temperature + "°F";
+    }
+
+    else if (fromUnit.value === "F" && toUnit.value === "K") {
+        result.textContent = (temperature - 32) * 5 / 9 + 273.15 + "°K";
+    }
+
+    else if (fromUnit.value === "K" && toUnit.value === "C") {
+        result.textContent = temperature - 273.15 + "°C";
+    }
+
+    else if (fromUnit.value === "K" && toUnit.value === "F") {
+        result.textContent = (temperature - 273.15) * 9 / 5 + 32 + "°F";
+    }
+
+    else if (fromUnit.value === "K" && toUnit.value === "K") {
+        result.textContent = temperature + "°K";
+    }
+
+    
+}
+
+convertButton.addEventListener("click", convertTemperature);
