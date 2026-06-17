f0c66cfc6ca9a2e5a7fc9fb45ebf02e1087f3803
Griffin <348875758@gapps.yrdsb.ca>
2026-06-17 14:24:38 -0400

    slight change made, just committing before starting final project

--- temperature.js
+++ temperature.js
@@ -8,39 +8,39 @@ function convertTemperature() {
     const temperature = Number(temperatureInput.value);
 
     if (fromUnit.value === "C" && toUnit.value === "C") {
-        result.textContent = temperature + "°C";
+        result.textContent = temperature.toFixed(2) + "°C";
     }
 
     else if (fromUnit.value === "C" && toUnit.value === "F") {
-        result.textContent = temperature * 9 / 5 + 32 + "°F";
+        result.textContent = (temperature * 9 / 5 + 32).toFixed(2) + "°F";
     }
 
     else if (fromUnit.value === "C" && toUnit.value === "K") {
-        result.textContent = temperature + 273.15 + "°K";
+        result.textContent = (temperature + 273.15).toFixed(2) + "°K";
     }
 
     else if (fromUnit.value === "F" && toUnit.value === "C") {
-        result.textContent = (temperature - 32) * 5 / 9 + "°C";
+        result.textContent = ((temperature - 32) * 5 / 9).toFixed(2) + "°C";
     }
 
     else if (fromUnit.value === "F" && toUnit.value === "F") {
-        result.textContent = temperature + "°F";
+        result.textContent = (temperature).toFixed(2) + "°F";
     }
 
     else if (fromUnit.value === "F" && toUnit.value === "K") {
-        result.textContent = (temperature - 32) * 5 / 9 + 273.15 + "°K";
+        result.textContent = ((temperature - 32) * 5 / 9 + 273.15).toFixed(2) + "°K";
     }
 
     else if (fromUnit.value === "K" && toUnit.value === "C") {
-        result.textContent = temperature - 273.15 + "°C";
+        result.textContent = (temperature - 273.15).toFixed(2) + "°C";
     }
 
     else if (fromUnit.value === "K" && toUnit.value === "F") {
-        result.textContent = (temperature - 273.15) * 9 / 5 + 32 + "°F";
+        result.textContent = ((temperature - 273.15) * 9 / 5 + 32).toFixed(2) + "°F";
     }
 
     else if (fromUnit.value === "K" && toUnit.value === "K") {
-        result.textContent = temperature + "°K";
+        result.textContent = (temperature).toFixed(2) + "°K";
     }
 
     
